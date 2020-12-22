95c3eb1a71a1b84532942af837638a069072715f
Ishant Dahiya <ishantdahiya@gmail.com>
2020-12-23 04:10:25 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,306 @@
+(function($) {
+ 
+// Preloader 
+    jQuery(window).on('load', function() {
+        jQuery("#status").fadeOut();
+        jQuery("#preloader").delay(200).fadeOut("slow");
+    });
+
+
+    // on ready function
+    $(document).ready(function() {
+		"use strict";	
+
+	// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) {       
+        $('#return-to-top').fadeIn(200);   
+    } else {
+        $('#return-to-top').fadeOut(200);  
+    }
+});
+$('#return-to-top').on('click', function() {     
+    $('body,html').animate({
+        scrollTop : 0                
+    }, 500);
+});
+
+
+//----------------------- MENU FIXED JS -----------------------//
+	
+	$(window).scroll(function(){
+		var window_top = $(window).scrollTop() + 1; 
+		if (window_top > 50) {
+			$('.main_menu_wrapper').addClass('menu_fixed animated fadeInDown');
+		} else {
+			$('.main_menu_wrapper').removeClass('menu_fixed animated fadeInDown');
+		}
+	});
+			
+			
+/*--- Responsive Menu Start ----*/
+
+$(".navbar-toggle").on("click", function(){
+  var w = $('#sidebar').width();
+  var pos = $('#sidebar').offset().left;
+ 
+  if(pos === 0){
+  $("#sidebar").animate({"left": -w}, "slow");
+  }
+  else
+  {
+  $("#sidebar").animate({"left": "0"}, "slow");
+  }
+  
+});
+
+$("#toggle_close").on("click", function(){
+  var w = $('#sidebar').width();
+  var pos = $('#sidebar').offset().left;
+ 
+  if(pos === 0){
+  $("#sidebar").animate({"left": -w}, "slow");
+  }
+  else
+  {
+  $("#sidebar").animate({"left": "0"}, "slow");
+  }
+  
+});
+		
+// -------------------------------------------------------------
+// Shuffle
+// -------------------------------------------------------------
+
+jQuery(window).on('load', function() {
+    /** this is come when complete page is fully loaded, including all frames, objects and images **/
+
+    if ($('#gridWrapper').length > 0) {
+     
+        /* initialize shuffle plugin */
+        var $grid = $('#gridWrapper');
+
+        $grid.shuffle({
+            itemSelector: '.portfolio-wrapper' // the selector for the items in the grid
+        });
+
+        /* reshuffle when user clicks a filter item */
+        $('#filter a').on('click', function (e) {
+            e.preventDefault();
+
+            // set active class
+            $('#filter a').removeClass('active');
+            $(this).addClass('active');
+
+            // get group name from clicked item
+            var groupName = $(this).attr('data-group');
+
+            // reshuffle grid
+            $grid.shuffle('shuffle', groupName );
+        });
+    }
+});
+
+
+		
+/*--- BASIC SLIDER 6 & 7 JS START ----*/
+  
+  (function( $ ) {
+
+	//Function to animate slider captions 
+	function doAnimations( elems ) {
+		//Cache the animationend event in a variable
+		var animEndEv = 'webkitAnimationEnd animationend';
+		
+		elems.each(function () {
+			var $this = $(this),
+				$animationType = $this.data('animation');
+			$this.addClass($animationType).one(animEndEv, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+	
+	//Variables on page load 
+	var $myCarousel = $('#carousel-example-generic'),
+		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+		
+	//Initialize carousel 
+	$myCarousel.carousel();
+	
+	//Animate captions in first slide on page load 
+	doAnimations($firstAnimatingElems);
+	
+	//Pause carousel  
+	$myCarousel.carousel('pause');
+	
+	
+	//Other slides to be animated on carousel slide event 
+	$myCarousel.on('slide.bs.carousel', function (e) {
+		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+		doAnimations($animatingElems);
+	});  
+
+	
+})(jQuery);	
+
+/*--- BASIC SLIDER 6 & 7 JS END ----*/
+
+
+
+$('.gc_filter_text').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1]
+			},
+			image: {
+				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+				titleSrc: function(item) {
+					return item.el.attr('title') + '<small></small>';
+				}
+			}
+		});
+		
+
+	
+//-------------------------------------------------------
+// counter Section
+//-------------------------------------------------------
+    (function () {
+            $('.counter_section').on('inview', function(event, visible, visiblePartX, visiblePartY) {
+                if (visible) {
+                    $(this).find('.timer').each(function () {
+                        var $this = $(this);
+                        $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                            duration: 2000,
+                            easing: 'swing',
+                            step: function () {
+                                $this.text(Math.ceil(this.Counter));
+                            }
+                        });
+                    });
+                    $(this).unbind('inview');
+                }
+            });
+
+    }());
+
+/*testimonial_slider_wrapper js */
+
+	$(document).ready(function() {
+              $('.testimonial_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:false,
+				smartSpeed: 1200,
+                responsiveClass: true,
+				navText : ['<i class="fa fa-angle-double-left" aria-hidden="true"></i>','<i class="fa fa-angle-double-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 1,
+                    nav: true
+                  },
+                  1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+    })
+	
+	
+	
+		/* team_slider js */
+	
+	$(document).ready(function() {
+              $('.team_slider .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:false,
+				smartSpeed: 1200,
+                responsiveClass: true,
+				navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 2,
+                    nav: true
+                  },
+                  1000: {
+                    items: 3,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+	
+		
+//Video Play
+	$('.play-trigger').magnificPopup({
+        type: 'iframe'
+      });
+      $.extend(true, $.magnificPopup.defaults, {
+        iframe: {
+          patterns: {
+            youtube: {
+              index: 'youtube.com/',
+              id: 'v=',
+              src: 'http://www.youtube.com/embed/%id%?autoplay=1'
+            }
+          }
+        }
+      });
+	
+// -------------------------------------------------------------
+//  client_slider
+// -------------------------------------------------------------
+
+   
+	$('.client_slider .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:false,
+                responsiveClass: true,
+				navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 3,
+                    nav: true
+                  },
+                  1000: {
+                    items: 5,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              });
+	
+	
+	
+	
+
+    });
+
+})(jQuery);
+	
